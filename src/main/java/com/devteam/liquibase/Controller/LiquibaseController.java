bc754c7ac408e61bf267db8d7bdaa27c720fe721
pasha <pashayevrauf62@gmail.com>
2022-02-08 12:09:33 +0400

    one to one

--- src/main/java/com/devteam/liquibase/Controller/LiquibaseController.java
+++ src/main/java/com/devteam/liquibase/Controller/LiquibaseController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * @author Rauf Pashayev
+ */
 @RestController
 @RequestMapping("/person")
 public class LiquibaseController {
